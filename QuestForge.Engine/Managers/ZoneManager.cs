2d8def0708071f09998ae7447eac88c394871844
eviedurdin <durej001@mymail.unisa.edu.au>
2026-04-15 10:56:44 +0930

    feat: ZoneManager with LinkedList

---
+++ QuestForge.Engine/Managers/ZoneManager.cs
@@ -0,0 +1,58 @@
+namespace QuestForge.Engine.Managers;
+using QuestForge.Engine.World;
+
+public class ZoneManager
+{
+    private LinkedList<Zone> _zones = new();
+    public Zone? CurrentZone { get; private set; }
+    public LinkedList<Zone> AddZone(Zone zone, Zone? prevZone, Zone? nextZone)
+    {
+        if (prevZone == null && nextZone == null)
+        {
+            if (_zones.Count > 0)
+                throw new InvalidOperationException("Can't add zone without neighbours if zones already exist");
+
+            _zones.AddFirst(zone);
+            CurrentZone = zone;
+            return _zones;
+        }
+
+        if (prevZone != null)
+        {
+            var prevNode = _zones.Find(prevZone);
+            if (prevNode == null)
+                throw new InvalidOperationException("prevZone not found in zone list");
+            _zones.AddAfter(prevNode, zone);
+        }
+        else
+        {
+            var nextNode = _zones.Find(nextZone!);
+            if (nextNode == null)
+                throw new InvalidOperationException("nextZone not found in zone list");
+
+            _zones.AddBefore(nextNode, zone);
+        }
+
+        return _zones;
+    }
+
+    public Zone? GetZone(string name)
+    {
+        foreach (var zone in _zones)
+        {
+            if (zone.Name == name)
+                return zone;
+        }
+        return null;
+    }
+
+    public bool SetCurrentZone(Zone zone)
+    {
+        if (_zones.Find(zone) == null)
+            return false;
+        CurrentZone = zone;
+        return true;
+    }
+
+    public LinkedList<Zone> GetZones() => _zones;
+}
--- QuestForge.Engine/obj/Debug/net10.0/QuestForge.Engine.AssemblyInfo.cs
+++ QuestForge.Engine/obj/Debug/net10.0/QuestForge.Engine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuestForge.Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d81b65af27e1809c30d512ccd927e3fdd5ffce17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c14c66526473d61dd7cce6039026a4a3182018e")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuestForge.Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuestForge.Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
